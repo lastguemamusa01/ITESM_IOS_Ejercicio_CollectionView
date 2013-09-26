d5015465b47c4744edd9e206d16199785a6b024e
Ma.Elena Melón Jareda <mmelon@MaElena-Melon-Jaredas-MacBook-Pro.local>
2013-09-26 10:15:36 -0500

    Initial Commit

---
+++ Ejercicio_CollectionView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Ejercicio_CollectionView
+//
+//  Created by Ma.Elena Melón Jareda on 9/26/13.
+//  Copyright (c) 2013 Ma.Elena Melón Jareda App Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
